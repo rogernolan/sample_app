16f450ca44fd24a26b5b279596f37172812eeaf4
Rog <rog@hatbat.net>
2011-12-21 18:44:28 +0000

    adding password - commit before christmas break.
    Signed-off-by: Rog <rog@hatbat.net>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
-  
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
+    
   validates :name,  :presence => true,
                     :length => { :maximum => 50 }
                     
@@ -8,4 +9,10 @@ class User < ActiveRecord::Base
   validates :email, :presence => true, 
                     :format => {:with => email_regex},
                     :uniqueness => { :case_sensitive => false }
+                    
+  # Automatically create the virtual attribute 'password_confirmation'. 
+  validates :password, :presence => true,
+                        :confirmation => true,
+                        :length => { :within => 6..40 }
+                     
 end
